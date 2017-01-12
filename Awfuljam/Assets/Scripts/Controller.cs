3c5ada784e4cf0d5860f4d96db76592023c8b8c6
PixelSplash <briamtec@gmail.com>
2017-01-11 22:37:34 -0600

    mouse

--- Awfuljam/Assets/Scripts/Controller.cs
+++ Awfuljam/Assets/Scripts/Controller.cs
@@ -16,7 +16,8 @@ public class Controller : MonoBehaviour {
     private AudioSource steps;
     // Use this for initialization
     void Start () {
-        Screen.showCursor = false;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
         cc = GetComponent<CharacterController>();
         mycam = GameObject.Find("Camera");
         steps = GetComponent<AudioSource>();
