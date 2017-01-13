9a8426316b8ab37bd8e6a1c461aa96703822071f
Jose Paulo Yock Fuentes <jyock1997@gmail.com>
2017-01-12 21:59:35 -0600

    Cambios en LightIntensity
    Se agrega el rango de la luz.

---
+++ Awfuljam/Assets/Scripts/LightIntensityRange.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightIntensityRange : MonoBehaviour {
+
+    public const float TIME = 30;
+    public const float TIME_RATE = 0.05f;
+    public const float MAX_LIGHT_INTENSITY = 3;
+    public const float MIN_LIGHT_INTENSITY = 0;
+
+    public const float MAX_LIGHT_RANGE = 3;
+    public const float MIN_LIGHT_RANGE = 1.5f;
+
+    public const float DISCOUNT_INTENSITY = MAX_LIGHT_INTENSITY / (TIME / TIME_RATE);
+    public const float DISCONUT_RANGE = (((MAX_LIGHT_RANGE - MIN_LIGHT_RANGE) * DISCOUNT_INTENSITY) / MAX_LIGHT_INTENSITY);
+
+    [Range(MIN_LIGHT_INTENSITY, MAX_LIGHT_INTENSITY)]
+    public float lightIntensity = MAX_LIGHT_INTENSITY;
+    [Range(MIN_LIGHT_RANGE, MAX_LIGHT_RANGE)]
+    public float lightRange = MAX_LIGHT_RANGE;
+    public int seconds;
+    public GameObject lightObj;
+
+    private Light light;
+
+    // Use this for initialization
+    void Start () {
+        light = lightObj.GetComponent<Light>();
+        InvokeRepeating("updateIntensity_Range", 1.0f, TIME_RATE);
+        InvokeRepeating("timer", 1.0f, 1.0f);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        light.intensity = lightIntensity;
+	}
+
+    public void updateIntensity_Range()
+    {
+        lightIntensity -= DISCOUNT_INTENSITY;
+        if (lightIntensity >= MAX_LIGHT_INTENSITY) lightIntensity = MAX_LIGHT_INTENSITY;
+        else if (lightIntensity <= MIN_LIGHT_INTENSITY) lightIntensity = MIN_LIGHT_INTENSITY;
+
+        lightRange -= DISCONUT_RANGE;
+        if (lightRange >= MAX_LIGHT_RANGE) lightRange = MAX_LIGHT_RANGE;
+        else if (lightRange <= MIN_LIGHT_RANGE) lightRange = MIN_LIGHT_RANGE;
+    }
+
+    public void setMaxIntensity_Range()
+    {
+        lightIntensity = MAX_LIGHT_INTENSITY;
+        lightRange = MAX_LIGHT_RANGE;
+    }
+    
+    public void timer()
+    {
+        seconds++;
+    }
+}
